27ee46364fc5a4b62f3bff82b9fd5baf1bcb420c
Daniel Martinez <daniel.martinez@ionix.cl>
2024-07-24 11:18:05 -0400

    Primer commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+/*En el root layout cualquier interfaz de usuario que se añada al layout raiz se compartira en todas las paginas de la aplicación.
+Se puede usar el root layout para modificar las etiquetas <html> y <body> y añadir metadatos.*/
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+//Se usara este archivo para mantener las fuentes que se usaran en la aplicacion.
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana=Lusitana({subsets: ['latin'], weight: ["400", "700"]});
